5b4d1807f7243e364e74473e142f9ed7d41ba42f
mykulyak <mykulyak@gmail.com>
2021-03-11 10:38:24 +0100

    feature3 in TS

--- packages/feature3/src/index.ts
+++ packages/feature3/src/index.ts
@@ -1,4 +1,6 @@
-export default function registerModule(context) {
+import type { Context } from '@pocfm/core/build';
+
+export default function registerModule(context: Context) {
   context.registerMenu('Feature 3', '/feature3');
   context.registerRoute('/feature3', () => import('./View'));
 }
